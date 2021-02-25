dd09f30f5d7d16a8bb2be6d5437c35fd8dbbf8fc
Helena Torres-Rocha <helena.torres-rocha@ocsbstudent.ca>
2021-02-25 00:24:52 +0000

    errors

--- script.js
+++ script.js
@@ -3,21 +3,20 @@ let answer = 0
 let diameter1 = 0
 let answer1 = 0
 
-document.getElementById('button').addEventListener('click', area1)
+document.getElementById('button').addEventListener('click', area)
 document.getElementById('button-2').addEventListener('click', circumference)
 
-function area1 () {
+function area () {
   diameter = document.getElementById('input').value
   diameter = parseInt(diameter)
-  answer = ((Math.PI * diameter * diameter)/4)    
+  answer = ((Math.PI * diameter * diameter) /4) 
   // pow(diameter,2) could be replaced by diameter * diameter
-  document.getElementById('answer').innerHTML = Math.round (answer)
+  document.getElementById('text1').innerHTML = Math.round(answer)
 }
 
 function circumference () {
-  circumference = parseInt(circumference)
   diameter1 = document.getElementById('input-2').value
   diameter1 = parseInt(diameter1)
   answer1 = (Math.Pi * diameter1)
-  document.getElementById('answer2').innerHTML = Math.round (answer1)
+  document.getElementById('text2').innerHTML = Math.round(answer1)
 }
